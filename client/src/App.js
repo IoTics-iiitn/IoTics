a0e8696440ce469ebea6d9318619aafe7f241880
Prateek Niket <prateekniket25@gmail.com>
2021-10-13 13:31:10 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
 
 const App = () => {
     return (
         <div>
-            <h1>App</h1>
+            {/* <h1>App</h1> */}
+            <Navbar />
         </div>
     )
 }
