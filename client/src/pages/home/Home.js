2f5054d196d0182d347c184a085f8ad2ebcf30a9
Prateek Niket <prateekniket25@gmail.com>
2021-10-13 22:28:16 +0530

    Add files via upload

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Hero from '../../components/hero/Hero'
+import About from '../about/About'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-            
+            <Hero />
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route exact path='/' />
+                    <Route path='/about-us' component={About} />
+                </Switch>
+            </Router>
         </div>
     )
 }
