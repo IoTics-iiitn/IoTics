85907a128dc7cd9174e0a3fcfa0d3fbca14d9193
Prateek Niket <prateekniket25@gmail.com>
2021-10-13 13:31:51 +0530

    Add files via upload

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,10 +1,22 @@
 import React from 'react'
+
+// importing the logo
+import image from './assets/logo.png'
+
 import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='nav-container'> 
+            <img className='logo' src={image} alt="img" />
+            <div className="nav-text">
+                <div className="home">
+                    <h1>Home</h1>
+                </div>
+                <div className="about">
+                    <h1>About Us</h1>
+                </div>
+            </div>
         </div>
     )
 }
