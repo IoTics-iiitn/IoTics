77e4e84e8ab7b50e8c56bb5fdab520add937232b
Prateek Niket <prateekniket25@gmail.com>
2021-10-20 14:54:52 +0530

    Add files via upload

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -9,6 +9,7 @@ import "./Navbar.css"
 const Navbar = () => {
 
     const [navbar, setNavbar] = useState(false)
+    const [show, setShow] = useState(false);
 
     const changeNavbar = () => {
         if (window.scrollY >= 100) {
@@ -23,18 +24,27 @@ const Navbar = () => {
 
     return (
         <div className={navbar ? 'nav-container scroll' : 'nav-container'}>
+            <div
+                onClick={() => setShow((prevState) => !prevState)}
+                className={`${show ? "hamburger-btn open" : "hamburger-btn"}`}
+            >
+                <div className="hamburger"></div>
+            </div>
+
             <img className={navbar ? 'logo scroll-logo' : 'logo'} src={image} alt="img" />
-            <div className="nav-text">
-                <NavLink exact to="/" className={isActive => "active" + (!isActive ? " unselected" : "")}>
-                    <div className="home">
-                        <h1>Home</h1>
-                    </div>
-                </NavLink>
-                <NavLink to="/about" className={isActive => "active" + (!isActive ? " unselected" : "")}>
-                    <div className="about">
-                        <h1>About Us</h1>
-                    </div>
-                </NavLink>
+            <div className={`${show ? "nav-menu" : "nav-menu hide"}`}>
+                <div className="nav-text">
+                    <NavLink exact to="/" className={isActive => "active" + (!isActive ? " unselected" : "")}>
+                        <div className="home">
+                            <h1>Home</h1>
+                        </div>
+                    </NavLink>
+                    <NavLink to="/about-us" className={isActive => "active" + (!isActive ? " unselected" : "")}>
+                        <div className="about">
+                            <h1>About Us</h1>
+                        </div>
+                    </NavLink>
+                </div>
             </div>
         </div>
     )
